3a0394d0d02b6feace70e60ec4c9c4515b7caa43
vagrant <vagrant@ant.host>
2016-12-05 12:35:46 +0000

    Initial commit.

--- provision.sh
+++ provision.sh
@@ -7,8 +7,16 @@ yum install -y vim git tree
 yum install -y java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
 
 JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
-wget http://www.us.apache.org/dist/ant/bin/binaries/$ANT_VER
-tar xvfvz $ANT_VER -C /opt
+cd /vagrant
+
+if [ -f /vagrant/$ANT_VER ]
+then
+        echo $ANT_VER exists
+else
+	wget http://www.us.apache.org/dist/ant/binaries/$ANT_VER 
+fi
+
+tar xvfvz /vagrant/$ANT_VER -C /opt
 ln -s /opt/$ANT /opt/ant
 sh -c 'echo ANT_HOME=/opt/ant >> /etc/environment'
 ln -s /opt/ant/bin/ant /usr/bin/ant
