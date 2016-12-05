02ea18ec472c000d0c34aee8c99b9ba41294f197
Mike Swartz <mikelswartz@gmail.com>
2016-12-05 07:55:13 -0500

    Added double quotes to force interpolation in provision file conditional test for zipped ant package

--- provision.sh
+++ provision.sh
@@ -9,9 +9,9 @@ yum install -y java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
 JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
 cd /vagrant
 
-if [ -f /vagrant/$ANT_VER ]
+if [ -f "/vagrant/$ANT_VER" ]
 then
-        echo $ANT_VER exists
+        echo "$ANT_VER exists"
 else
 	wget http://www.us.apache.org/dist/ant/binaries/$ANT_VER 
 fi
